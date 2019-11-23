1616f6ae0742783ed4820480edd0785a741a938c
nolanG <nolanrgiles@gmail.com>
2019-11-23 13:22:51 -0500

    dealCards() function created, now can deal a specific number of cards

--- js/mainGame.js
+++ js/mainGame.js
@@ -4,6 +4,8 @@ let deckId;
 // let getCardsURL = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=` //add the # of cards you want from the deck (52 = 1 deck, 312 = 6 decks)
 let deck; // store the full 52 card deck here. (array of card objects)
 let gameState = "start"; //Should be a string equating to the current gameState such as "over", "bet", "inHand", "outHand" & "start"
+let dealerHand = [];
+let playerHand = [];
 //BACK OF CARD IMAGE = 226px x 314px
 //These game states will allow playGame() to be called over and over and shooting to the appropriate section of the game.
 //start is the game state where the game has yet to begin and needs the player to choose to start playing, it should show a New game button and nothing else. It should also call the gameInit() function?
@@ -39,14 +41,14 @@ async function newDeck() { //WHEN CALLED: needs to be run inside of another asyn
   let decksIdObj = await axios.get(`${getNewDecksURL}6`);
   deckId = decksIdObj.data.deck_id;
   console.log(`Six new decks created! The deckId = ${deckId}`);
-  deck = await drawCards(312); //Fills the global deck array up with the full 6 decks, and they are already shuffled.
+  deck = await drawCards(312); //Fills the global deck array up with the full 6 decks, and they are ALREADY SHUFFLED, not necessary to follow up with shuffleDeck.
 }
 
 
 //This is the function that actually fills the global deck variable with the cards. Not to be confused with dealCards()
 //Takes argument that dictates how many cards to fill the deck array with, almost always this will be the full reset of 312 cards, however if we only wanted a single deck of 52 for example, that can be passed in and the deck array will only be 1 full deck large.
-async function drawCards(num) {
-  let decksObj = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${num}`);
+async function drawCards(numOfCards) {
+  let decksObj = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${numOfCards}`);
   return decksObj.data.cards;
 }
 
@@ -58,39 +60,41 @@ async function shuffleDeck() {
 }
 
 
+//dealCards(number of cards)
+//returns an array filled with the number of cards given in the parameter
+function dealCards(numOfCards) {
+  let cards = [];
+  cards = deck.splice(0, numOfCards);// splice returns cards starting at the first variable, number of cards to return. So if first arg is 0, returning starting at the start of the array.  If I wanted to return the final 2 cards I would use splice(-numofcards, numofcards) (negative2, 2)
+  return cards;
+}
+
+//dealHand
+//using dealCards() applies the appropriate amount of cards to each hand, 2 each.
 
 
+//clearHand()
+//take both hand arrays that have been played in and reset them
 
+ 
 async function testRun() { //temporary fn to call subsets of the game for testing 
   await newDeck();
-  await shuffleDeck();
+  // await shuffleDeck();
+  console.log(dealCards(2));
 }
 testRun();
-
-  //clearHand()
-  //take both hand arrays that have been played in and reset them
-
   
-  //dealCards(number of cards)
-  //returns the number of cards (pops) from the deck array
 
-
-  //dealHand
-  //using dealCards() applies the appropriate amount of cards to each hand, 2 each.
-
-
-
-  //playGame()
-  //where most game logic will take place
-  //just a series of if elses given the current gameState string, run that code.
-  //all gameState variable changes will occur in other functions, then recall playGame() and playGame will jump to the appropriate
-  //part due to it checking the gameState
-  //call dealHand
-  //once the player can see their cards,  change gameState to "bet"
-  //make betOptions appear, this should be a div containing the buttons that take from totalMoney and sets currentBet
-  //
-  //make gameOptions appear, this should be a div containing the buttons for hit and stand (keep it simple at the start)
-  //
+//playGame()
+//where most game logic will take place
+//just a series of if elses given the current gameState string, run that code.
+//all gameState variable changes will occur in other functions, then recall playGame() and playGame will jump to the appropriate
+//part due to it checking the gameState
+//call dealHand
+//once the player can see their cards,  change gameState to "bet"
+//make betOptions appear, this should be a div containing the buttons that take from totalMoney and sets currentBet
+//
+//make gameOptions appear, this should be a div containing the buttons for hit and stand (keep it simple at the start)
+//
 function playGame() {
     
   if (gameState === "bet") {
